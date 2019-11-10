bb5eb4043ec0c13cb6a95e599cc46a9ac06c453e
Ruben Waterman <rubenwaterman@me.com>
2019-11-10 12:54:06 +0000

    fix typo

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -424,13 +424,13 @@ class item
         }
 
         if (($this -> name == 'Total') {
-            $left = str_pad($this -> name, $leftCols);)
+            $left = str_pad($this -> name, $leftCols);
         } elseif ($this -> quantity > 0) {
             $left = str_pad($this -> quantity . ' x ' . $this -> name, $leftCols);
         } else {
             $left = str_pad($this -> name, $leftCols);
         }
-        
+
         $sign = ($this -> dollarSign ? 'EUR ' : '');
         $right = str_pad($sign . $this -> price, $rightCols, ' ', STR_PAD_LEFT);
         return "$left$right\n";
