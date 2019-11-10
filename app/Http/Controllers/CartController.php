0bc439bb62e0404d54e55150b7c301d30d44ea9a
Ruben Waterman <rubenwaterman@me.com>
2019-11-10 12:06:33 +0000

    add helper classes

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -391,8 +391,69 @@ class CartController extends Controller {
     }
 }
 
+class item
+{
+    private $quantity;
+    private $name;
+    private $price;
+    private $dollarSign;
 
+    public function __construct($name = '', $price = '', $quantity = 0, $dollarSign = false)
+    {
+        $this -> quantity = $quantity;
+        $this -> name = $name;
+        $this -> price = $price;
+        $this -> dollarSign = $dollarSign;
+    }
 
+    public function __toString()
+    {
+        $rightCols = 10;
+        $leftCols = 38;
+        if ($this -> dollarSign) {
+            $leftCols = $leftCols / 2 - $rightCols / 2;
+        }
+        $left = str_pad($this -> quantity . ' x ' . $this -> name, $leftCols) ;
 
+        $sign = ($this -> dollarSign ? 'EUR ' : '');
+        $right = str_pad($sign . $this -> price, $rightCols, ' ', STR_PAD_LEFT);
+        return "$left$right\n";
+    }
+}
+
+function callAPI($method, $url, $data){
+    $curl = curl_init();
+ 
+    switch ($method){
+       case "POST":
+          curl_setopt($curl, CURLOPT_POST, 1);
+          if ($data)
+             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+          break;
+       case "PUT":
+          curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+          if ($data)
+             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+          break;
+       default:
+          if ($data)
+             $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+ 
+    // OPTIONS:
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+       'APIKEY: 111111111111111111111',
+       'Content-Type: application/json',
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+ 
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if(!$result){die("Connection Failure");}
+    curl_close($curl);
+    return $result;
+ }
 
 
