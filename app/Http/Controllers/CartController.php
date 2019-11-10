bb8c4cc0bb930bb561339cb8edacdd3e78b918f9
Ruben Waterman <rubenwaterman@me.com>
2019-11-10 12:36:44 +0000

    forgot semicolon

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -424,7 +424,7 @@ class item
         }
 
         if ($this -> quantity > 0) {
-            $quant = $this -> quantity
+            $quant = $this -> quantity;
         }
         
         $left = str_pad($quant . ' x ' . $this -> name, $leftCols) ;
