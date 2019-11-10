de1e9f8bb95590e63d4e3c108414d656fc677010
Ruben Waterman <rubenwaterman@me.com>
2019-11-10 12:18:56 +0000

    fix path of logo

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -273,7 +273,7 @@ class CartController extends Controller {
         \QRcode::png($lnurl, "test.png", 'L', 5, 0);
 
         $img = EscposImage::load("test.png");
-        $logo = EscposImage::load("resources/rawbtlogo.png", false);
+        $logo = EscposImage::load("blocklogo.png", false);
 
 
         $connector = new FilePrintConnector("/dev/usb/lp0");
